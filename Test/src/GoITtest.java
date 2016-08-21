0cb3b9b9d3186666cc10d9b1ee607b205ea865fb
victoria123654 <krasikova.vg@gmail.com>
2016-08-21 11:19:59 -0400

    test

---
+++ Test/src/GoITtest.java
@@ -0,0 +1,16 @@
+import java.text.MessageFormat;
+
+
+public class GoITtest {
+
+    public static void main(String[] args) {
+        System.out.println("have no idea how to use rest of the commands");
+        System.out.printf("JC");
+        System.out.println("true = " + true);
+        if (!("7" == null)) {
+
+        }
+        {
+        }
+    }
+}
---
+++ Test/src/test11.java
@@ -0,0 +1,5 @@
+/**
+ * Created by victoriakrasikova on 8/21/16.
+ */
+public class test11 {
+}
