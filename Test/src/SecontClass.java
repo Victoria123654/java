e9e88cc429f7429261559044ae0ce42e8382cc5a
victoria123654 <krasikova.vg@gmail.com>
2016-08-21 11:49:34 -0400

    HT

--- Test/src/SecontClass.java
+++
@@ -1,7 +0,0 @@
-
-public class SecontClass {
-
-    public static void main(String[] args) {
-        System.out.println("America: one million yogurt flavors, yet only two viable Presidential candidates.");
-    }
-}
